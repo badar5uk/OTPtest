3c8d34eed360e6733534e1f9eed11b1c84cef022
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-03-25 12:22:30 +0400

    adjusted application properties to update and adjusted model with the proper table name

--- src/main/java/badar/app/otp/Models/OTPRecord.java
+++ src/main/java/badar/app/otp/Models/OTPRecord.java
@@ -1,17 +1,15 @@
 package badar.app.otp.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.Instant;
 
 @Entity
+@Table(name = "otp_record")
 public class OTPRecord {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String phoneNumber;
