4ebe10b52df04f5809062224d5c1d2789c7e2de2
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-03-25 10:03:40 +0400

    adjusted OTPRecord

--- src/main/java/badar/app/otp/Models/OTPRecord.java
+++ src/main/java/badar/app/otp/Models/OTPRecord.java
@@ -1,10 +1,25 @@
 package badar.app.otp.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.Instant;
 
+@Entity
 public class OTPRecord {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String phoneNumber;
+
     private String otp;
+
+    private Instant timestamp;
+
 
     public Instant getTimestamp() {
         return timestamp;
@@ -22,6 +37,19 @@ public class OTPRecord {
         this.otp = otp;
     }
 
-    private Instant timestamp;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
