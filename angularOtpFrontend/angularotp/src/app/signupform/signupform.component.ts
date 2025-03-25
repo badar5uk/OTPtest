337ad96ba2eb11a7d115059207878e646639debb
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-03-25 13:31:15 +0400

    Added basic angular structure for frontend

---
+++ angularOtpFrontend/angularotp/src/app/signupform/signupform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signupform',
+  imports: [],
+  templateUrl: './signupform.component.html',
+  styleUrl: './signupform.component.css'
+})
+export class SignupformComponent {
+
+}
