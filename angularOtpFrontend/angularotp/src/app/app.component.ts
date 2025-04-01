bc3d01f4c4ac9d4b02c2f53c2b39c9dba79e2273
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-04-01 10:00:04 +0400

    Added NgbModule

--- angularOtpFrontend/angularotp/src/app/app.component.ts
+++ angularOtpFrontend/angularotp/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignupformComponent } from './signupform/signupform.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SignupformComponent, ReactiveFormsModule],
+  imports: [RouterOutlet,SignupformComponent, ReactiveFormsModule, NgbModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angularotp';
+  title = "OTP";
 }
--- angularOtpFrontend/angularotp/src/main.ts
+++ angularOtpFrontend/angularotp/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(NgbModule),
+  ]
+})
   .catch((err) => console.error(err));
